0895fa847f3f6dcce8ecad92ff09daf57c3eec84
Mario Mata <mmatas1300@gmail.com>
2024-08-22 17:09:03 -0600

    Suma

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,8 +1,34 @@
+import java.util.List;
+
 public class Calculadora implements ICalculadora {
+
+    public boolean verificar(int a, int b) throws Exception {
+
+        if (Integer.isNaN(a) || Integer.isNaN(b)) {
+            System.out.println("A");
+        }
+
+    }
 
     @Override
     public int sumar(int a, int b) {
         return a+b;
+    }
+
+    public int sumar(int... array){
+        int suma = 0;
+
+        if(array.length == 0){
+
+        } else{
+
+        }
+
+        for(int a:array) {
+            suma += a;
+        }
+
+        return a;
     }
 
     @Override
@@ -18,15 +44,12 @@ public class Calculadora implements ICalculadora {
     @Override
     public int dividir(int a, int b)  {
         int div = 0;
-
         try {
             div = a/b;
         }
         catch (ArithmeticException e){
             System.out.println("No");
         }
-
         return div;
-
     }
 }
--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        int i = 5;
+        do{
+            switch(){
 
+            }
+            System.out.println("holis");
+            i--;
+        }while(i>0);
     }
 
 }
