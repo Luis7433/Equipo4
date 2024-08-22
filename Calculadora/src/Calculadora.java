b9feca24ccba41ab1c014ede45a1443f111b3725
Mario Mata <mmatas1300@gmail.com>
2024-08-22 16:27:18 -0600

    operaciones

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,6 +1,32 @@
-public interface Calculadora {
-    int sumar();
-    int restar();
-    int multiplicar();
-    int dividir();
+public class Calculadora implements ICalculadora {
+
+    @Override
+    public int sumar(int a, int b) {
+        return a+b;
+    }
+
+    @Override
+    public int restar(int a, int b) {
+        return a-b;
+    }
+
+    @Override
+    public int multiplicar(int a, int b)  {
+        return a*b;
+    }
+
+    @Override
+    public int dividir(int a, int b)  {
+        int div = 0;
+
+        try {
+            div = a/b;
+        }
+        catch (ArithmeticException e){
+            System.out.println("No");
+        }
+
+        return div;
+
+    }
 }
---
+++ Calculadora/src/ICalculadora.java
@@ -0,0 +1,6 @@
+public interface ICalculadora {
+    int sumar(int a, int b) throws Exception;
+    int restar(int a, int b) throws Exception;
+    int multiplicar(int a, int b) throws Exception;
+    int dividir(int a, int b) throws Exception;
+}
