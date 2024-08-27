50e2c4d9aa65e1dc053425419a17a482b00cd339
hugo <hugocastanedahernandez1@gmail.com>
2024-08-27 09:50:33 -0600

    Se agregan las funciones en el Main

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -1,97 +1,72 @@
 import java.util.Scanner;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		
+		Calculadora calc = new Calculadora();
+		Scanner scanner = new Scanner(System.in);
+		
+		double num1 = getNum(scanner, "Ingrese el primer número (puede ser entero o decimal): ");
+		double num2 = getNum(scanner, "Ingrese el segundo número (puede ser entero o decimal): ");
+		
+		char operacion = getOper(scanner);double resultado = 0.0;
 
-    public static void main(String[] args) {
+		try {
+			switch (operacion) {
+			case '+':
+				resultado = calc.sumar(num1, num2);
+				break;
+			case '-':
+				resultado = calc.restar(num1, num2);
+				break;
+			case '*':
+				resultado = calc.multiplicar(num1, num2);
+				break;
+			case '/':
+				resultado = calc.dividir(num1, num2);
+				break;
+			default:
+				System.out.println("Operación no válida");
+				return;
+			}
+			System.out.println("El resultado es: " + resultado);
+			
+		} catch (ArithmeticException e) {
+			System.out.println(e.getMessage()); // Manejo de la excepción de la división
+		}
+	}
 
-        Calculadora calc = new Calculadora();
+	public static double getNum(Scanner scanner, String mensaje) {
+		double numero = 0.0;
+		boolean validInput = false;
+		
+		while (!validInput) {
+			try {
+				System.out.println(mensaje);
+				numero = Double.parseDouble(scanner.nextLine().trim());
+				validInput = true;
+			} catch (NumberFormatException e) {
+				System.out.println("Número inválido. Por favor, ingrese un número válido.");
+			}
+		}
+		return numero;
+	}
 
-        Scanner scanner = new Scanner(System.in);
+	public static char getOper(Scanner scanner) {
+		char operacion = ' ';
+		boolean validInput = false;
+		
+		while (!validInput) {
+			System.out.println("Seleccione la operación (+, -, *, /): ");
+			operacion = scanner.nextLine().trim().charAt(0);
+			if (operacion == '+' || operacion == '-' || operacion == '*' || operacion == '/') {
+				validInput = true;
+			} else {
+				System.out.println("Operación no válida. Por favor, ingrese una operación válida (+, -, *, /).");
+			}
+		}
+		return operacion;
+	}
 
-        double num1 = 0.0, num2 = 0.0;
-
-        int num3 = 0, num4 = 0;
-
-        char operacion = ' ';
-
-        boolean validInput = false; // flag para la validacion de los numeros
-
-        while (!validInput) { // While que lee el 1er numero
-
-            try {
-                System.out.println("Ingrese el primer número (puede ser entero o decimal): ");
-
-                num1 = Double.parseDouble(scanner.nextLine().trim());
-
-                validInput = true;
-
-            } catch (NumberFormatException e) {
-
-                System.out.println("Número inválido. Por favor, ingrese un número válido.");
-            }
-        }
-
-        validInput = false; // Resetear el flag para el 2do numero
-
-        while (!validInput) { // While que lee el 2do numero
-
-            try {
-                System.out.println("Ingrese el segundo número (puede ser entero o decimal): ");
-
-                num2 = Double.parseDouble(scanner.nextLine().trim());
-
-                validInput = true;
-
-            } catch (NumberFormatException e) {
-
-                System.out.println("Número inválido. Por favor, ingrese un número válido.");
-            }
-        }
-
-        validInput = false; // Resetear el flag para la operación
-
-        // While para leer el tipo de operacion
-        while (!validInput) {
-
-            System.out.println("Seleccione la operación (+, -, *, /): ");
-
-            operacion = scanner.nextLine().trim().charAt(0);
-
-            if (operacion == '+' || operacion == '-' || operacion == '*' || operacion == '/') {
-
-                validInput = true;
-
-            } else {
-                System.out.println("Operación no válida. Por favor, ingrese una operación válida (+, -, *, /).");
-
-            }
-        }
-
-        double resultado = 0.0;
-
-        try {
-            switch (operacion) {
-                case '+':
-                    resultado = calc.sumar(num1, num2);
-                    break;
-                case '-':
-                    resultado = calc.restar(num1, num2);
-                    break;
-                case '*':
-                    resultado = calc.multiplicar(num1, num2);
-                    break;
-                case '/':
-                    resultado = calc.dividir(num1, num2);
-                    break;
-                default:
-                    System.out.println("Operación no válida");
-                    return;
-            }
-            System.out.println("El resultado es: " + resultado);
-
-        } catch (ArithmeticException e) {
-
-            System.out.println(e.getMessage()); // Manejo de la excepción de la division
-        }
-    }//metodo main
-}//class Main
+}
